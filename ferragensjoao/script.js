12f7c82c48df411241f209093cc1fae25df02c6e
isa <maria.i.ferreira10@aluno.senai.br>
2025-04-15 11:07:55 -0500

    continuacao

---
+++ ferragensjoao/script.js
@@ -0,0 +1,21 @@
+let martelo = 19.99;
+
+//pegar o valor do campo produto
+
+//pegar o valor do campo qtd
+const precos={//dicionrio
+    "parafuso": 10,
+    "martelo": 20,
+    "chave de fenda": 30
+};
+
+a = [1, 56, 6, 10, 8]
+function p(){
+    let escolhaproduto = document.getElementById("produto").value;
+    console.log(escolhaproduto);
+    let quantidadedeproduto = document.getElementById("Quantidade").value;
+    console.log(quantidadedeproduto)
+
+    
+    
+}
