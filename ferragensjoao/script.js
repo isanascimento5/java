fe2ed20f8b4a331ab406b2c3910ee11c28fd630d
isa <maria.i.ferreira10@aluno.senai.br>
2025-04-24 11:00:27 -0500

    mais alguma coisa

--- ferragensjoao/script.js
+++ ferragensjoao/script.js
@@ -1,19 +1,37 @@
-let martelo = 19.99;
+let martelo = 20.90;
 
 //pegar o valor do campo produto
 
 //pegar o valor do campo qtd
 const precos={//dicionrio
-    "parafuso": 10,
-    "martelo": 20,
-    "chave de fenda": 30
+    "parafuso": 10.99,
+    "martelo": 20.90,
+    "chave de fenda": 30.80
 };
 
 
-function p(){
-    let escolhaproduto = document.getElementById("produto").value;
-    let quantidadedeproduto = document.getElementById("Quantidade").value;
-    console.log(precos[escolhaproduto]*quantidadedeproduto);
+const estoque={
+    "parafuso" : 100,
+    "martelo" :50,
+    "chave de fenda" : 40,
+};
+
+
+function calcularPreco(){
+
+    let qtd = document.getElementById("quantidade").value;
+    let escolha = document.getElementById("Quantidade").value;
+    let valortotal = qtd * precos[escolha]
     
+
+    if(estoque[escolha] - qtd >=0){
+        document.getElementById("resultado").innerHTML = valortotal.toFixed(2);
+        estoque(escolha) -=qtd;
+        window.alert("compra realizada com sucesso" +  valortotal.toFixed(2))
+    }else{
+        window.alert("ESTOQUE ESGOTADO")
+    }
+    console.log(estoque[escolhaproduto]);
+
     
 }
